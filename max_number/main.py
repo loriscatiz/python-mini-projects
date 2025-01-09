091bb28cf271f355732149235de560caf01aba22
loriscatiz <loriscatiz@gmail.com>
2025-01-09 15:27:28 +0100

    add max number

---
+++ max_number/main.py
@@ -0,0 +1,22 @@
+
+
+def main():
+
+    def getValidInput(prompt):
+        while True:
+            try: return float(input(prompt))
+            except ValueError: print("Please, insert a valid value")
+
+    x1 = getValidInput("Insert first number: ")
+    x2 = getValidInput("Insert second number: ")
+
+    if x1 > x2: 
+        print(x1, " is greater than ", x2)
+    elif x1 < x2: 
+        print(x2, " is greater than ", x1)
+    else: 
+        print(x1, " and ", x2, " are equal")
+
+
+if __name__ == "__main__":
+    main()
