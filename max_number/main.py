703acedb9ce0157f3487045d2e292fc35a72a566
loriscatiz <loriscatiz@gmail.com>
2025-01-10 17:53:50 +0100

    remove double spaces

--- max_number/main.py
+++ max_number/main.py
@@ -9,11 +9,11 @@ def main():
     x2 = getValidInput("Insert second number: ")
 
     if x1 > x2: 
-        print(x1, " is greater than ", x2)
+        print(x1, "is greater than", x2)
     elif x1 < x2: 
-        print(x2, " is greater than ", x1)
+        print(x2, "is greater than", x1)
     else: 
-        print(x1, " and ", x2, " are equal")
+        print(x1, "and", x2, "are equal")
 
 if __name__ == "__main__":
     main()
