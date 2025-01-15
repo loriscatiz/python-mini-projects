f1b1ada88583c65b9b0082182da3b1aaa99fe25e
loriscatiz <loriscatiz@gmail.com>
2025-01-15 17:49:11 +0100

    russian roulette and matchstick game final

--- russian-roulette/main.py
+++ russian-roulette/main.py
@@ -97,32 +97,61 @@ def get_bool() -> bool:
 
                 return retvalue
 
-                 
 
-def start():
-    print("Welcome user, let's play a game \nDo you know roussian roulette?")
-    knows_game: bool = get_bool()
-    print("Good") if knows_game == True else print("We decide the number of chambers and the number of bullets inside a revolver.\nWe load the revolver and spin the cylinder. Then each turn a player shoots the revolver. Last one alive wins.")
 
-    print("How many chambers?")
-    chambers = get_int_greater_or_equal_than(1)
-    print("How many bullets?")
-    bullets = get_int_in_range(0, chambers)
-     # Create a list with bullets (True) and empty chambers (False)
-    if bullets == 0:
-         print("You chose peace, congrats")
-         return
-    revolver = [True] * bullets + [False] * (chambers - bullets)
-    shuffle(revolver)
-    print(revolver)
-     
-def game():
-    
+
+
+def engine(revolver: list[bool], actual_turn: int, actual_player: str, index: int, player_turn: int, player_1: str, player_2: str):
+    while revolver[index] == False:
+        print(f"{actual_turn}. {actual_player}  click")
+        index += 1
+        actual_turn += 1
+        player_turn = 1 if actual_player == player_2 else 2
+        actual_player = player_1 if player_turn == 1 else player_2
+    else:
+        print(f"{actual_turn}. {actual_player}  BANG!")
+        print(f"{actual_player} Dies :(")
+        winner = player_1 if actual_player == player_2 else player_2
+        print(f"{winner} Lives :)")
 
 
 
 def main():
-    start()
+    min_username_lenght: int = 2
+    max_username_lenght: int = 8
+
+    print("Insert first player username: ")
+    player_1: str = get_string_in_range(min_username_lenght, max_username_lenght)  # Get valid username for player 1
+
+    print("Insert second player username: ")
+    player_2: str = get_string_in_range(min_username_lenght, max_username_lenght)  # Get valid username for player 2
+    print("Welcome players, let's play a game \nDo you know roussian roulette?")
+    knows_game: bool = get_bool()
+    print("Good") if knows_game == True else print("We decide the number of chambers and the number of bullets inside a revolver.\nWe load the revolver and spin the cylinder. Then each turn a player shoots the revolver. Last one alive wins.")
+
+    print("How many chambers in the cylinder?")
+    chambers = get_int_greater_or_equal_than(1)
+    print("How many bullets?")
+    bullets = get_int_in_range(0, chambers)
+    if bullets == 0:
+         print("You chose peace, congrats")
+         return
+     # Create a list with bullets (True) and empty chambers (False)
+    revolver = [True] * bullets + [False] * (chambers - bullets)
+    shuffle(revolver)
+
+    actual_turn: int = 1
+    print("Let's see who starts")
+    player_turn: int = randint(1, 2)
+
+    if player_turn == 1:
+        actual_player = player_1
+    else:
+        actual_player = player_2
+
+    print(f"{actual_player}'s gonna shoot first")
+
+    engine(revolver, actual_turn, actual_player, 0, player_turn, player_1, player_2)
 
 if __name__ == "__main__":
     main()
